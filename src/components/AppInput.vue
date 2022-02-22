6d6c03a8a08b346e35e0ae89725ee861dc2aebca
Danila Hlazau <danila.hlazau@itechart-group.com>
2022-02-22 17:57:38 +0300

    Add component for app input

---
+++ src/components/AppInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    class="app-input"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+  export default {
+    name: 'AppInput',
+    props: ['modelValue'],
+    emits: ['onUpdate:modelValue']
+  }
+</script>
+
+<style scoped>
+  .app-input {
+    padding: 10px 8px;
+    margin: 8px 0;
+  }
+</style>
